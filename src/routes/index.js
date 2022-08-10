e88546410818acbd420c2bb2054df8ab986b6606
Luzin7 <lvictor.dutra@gmail.com>
2022-08-10 08:00:58 -0300

    feat: Add admin page path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,30 @@
 import React, { lazy, Suspense } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import ProtectRoutes from "../utils/protectRoutes";
+import {
+  ProtectDefaultRoutes,
+  ProtectAdminRoutes,
+} from "../utils/protectRoutes";
 import * as Path from "../utils/pathNames";
 
 import NotFound from "../components/NotFound";
-import Redirect from "./../pages/Redirect";
 import Loading from "./../components/Loading";
 
+const Redirect = lazy(() => import("../pages/Redirect"));
 const Register = lazy(() => import("../pages/Register"));
-const Home = lazy(() => import("../pages/Home"));
+const DefaultHome = lazy(() => import("../pages/DefaultHome"));
 const Movie = lazy(() => import("../pages/Movie"));
+const AdminHome = lazy(() => import("../pages/AdminHome"));
 
 function Index() {
   return (
     <Router>
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route element={<ProtectRoutes />}>
-            <Route exact path={Path.MOVIES} element={<Home />} />
+          <Route element={<ProtectDefaultRoutes />}>
+            <Route exact path={Path.MOVIES} element={<DefaultHome />} />
+          </Route>
+          <Route element={<ProtectAdminRoutes />}>
+            <Route exact path={Path.ADMIN} element={<AdminHome />} />
           </Route>
           <Route exact path={Path.HOME} element={<Redirect />} />
           <Route exact path={Path.REGISTER} element={<Register />} />
