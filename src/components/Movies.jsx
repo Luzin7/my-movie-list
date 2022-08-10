c1a25bbdeebb7e17995583953b38c3e8bd8c39c3
Luzin7 <lvictor.dutra@gmail.com>
2022-08-10 08:07:15 -0300

    feat: Add movies sugestion and search bar
    - Add a serch bar to search some movies
    - Add random movie sugestion

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,3 +1,5 @@
+import { React, useState } from "react";
+
 import { Link } from "react-router-dom";
 
 import { MOVIES, NEXT_MOVIES } from "../data/movies";
@@ -7,9 +9,17 @@ import "../styles/movies/moviesWatched.css";
 import "../styles/movies/nextMovies.css";
 
 function Movies() {
+  const [search, setSearch] = useState("");
   const moviesLenght = MOVIES.length;
   const nextMoviesLenght = NEXT_MOVIES.length;
   const totalMovies = moviesLenght + nextMoviesLenght;
+
+  const movies = MOVIES.filter((e) =>
+    e.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  var randomMovieRecomendation =
+    NEXT_MOVIES[Math.floor(Math.random() * NEXT_MOVIES.length)];
 
   function goTopBtn() {
     window.scrollTo(0, 0);
@@ -17,56 +27,73 @@ function Movies() {
 
   return (
     <main className="movies">
-      {/* <section className="movies__search">
+      <section className="search__movies">
         <div className="search">
-          <input type="search" placeholder="Filme" />
-        </div>
-      </section> */}
-      <section className="movies__watched">
-        <h1 className="movies__title">Filmes avaliados</h1>
-        <span id="total-watched">
-          {moviesLenght}/{totalMovies}
-        </span>
-        <div className="movies__content">
-          <ul className="movies__cards">
-            <div className="slider">
-              <div className="slides">
-                {MOVIES.map((movie) => (
-                  <li key={movie.id} className="movie__card watched">
-                    <Link to={`movie/${movie.id}`}>
-                      <h2 className="movie__title">{movie.name}</h2>
-                      <div className="movies__img">
-                        <img
-                          className="movie__img"
-                          src={movie.img}
-                          alt={`Capa do filme ${movie.name}`}
-                        />
-                      </div>
-                      <p className="movie__desc-text">{movie.description}</p>
-                    </Link>
-                  </li>
-                ))}
-              </div>
-            </div>
-          </ul>
+          <label className="search__label">Pesquise um filme</label>
+          <input
+            className="search__input"
+            type="search"
+            placeholder="Filme"
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
       </section>
+      <section className="movies__watched">
+        {movies.length > 0 ? (
+          <>
+            <h1 className="movies__title">Filmes avaliados</h1>
+            <span id="total-watched">
+              {moviesLenght}/{totalMovies}
+            </span>
+            <div className="movies__content">
+              <ul className="movies__cards">
+                <div className="slider">
+                  <div className="slides">
+                    {movies.map((movie) => (
+                      <li key={movie.id} className="movie__card watched">
+                        <Link to={`movie/${movie.id}`}>
+                          <h2 className="movie__title">{movie.name}</h2>
+                          <div className="movies__img">
+                            <img
+                              className="movie__img"
+                              src={movie.img}
+                              alt={`Capa do filme ${movie.name}`}
+                            />
+                          </div>
+                          <p className="movie__desc-text">
+                            {movie.description}
+                          </p>
+                        </Link>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              </ul>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="movies__title">Não avaliamos esse filme ainda</h1>
+          </>
+        )}
+      </section>
       <section className="next__movies">
-        <h1 className="next movies__title-sec">Próximos filmes</h1>
+        <h1 className="next movies__title-sec">Veja também</h1>
         <div className="movies__content">
           <ul className="movies__cards">
-            {NEXT_MOVIES.map((movie) => (
-              <li key={movie.id} className="movie__card unwatched">
-                <h2 className="movie__title">{movie.name}</h2>
-                <div className="movies__img">
-                  <img
-                    className="movie__img"
-                    src={movie.media}
-                    alt={`Capa do filme ${movie.name}`}
-                  />
-                </div>
-              </li>
-            ))}
+            <li
+              key={randomMovieRecomendation.id}
+              className="movie__card unwatched"
+            >
+              <h2 className="movie__title">{randomMovieRecomendation.name}</h2>
+              <div className="movies__img">
+                <img
+                  className="movie__img"
+                  src={randomMovieRecomendation.media}
+                  alt={`Capa do filme ${randomMovieRecomendation.name}`}
+                />
+              </div>
+            </li>
           </ul>
         </div>
       </section>
