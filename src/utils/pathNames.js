3c43cb002b078a13eeeed582bdb27cdd23229b50
Luzin7 <lvictor.dutra@gmail.com>
2022-08-10 07:57:18 -0300

    docs: Fix path

--- src/utils/pathNames.js
+++ src/utils/pathNames.js
@@ -1,5 +1,5 @@
 export const HOME = "/";
 export const MOVIES = "/movies";
-export const MOVIE = "/movies/movie/:id" || "/admin/movie/:id";
+export const MOVIE = "/movies/movie/:id";
 export const REGISTER = "/register";
 export const ADMIN = "/admin";
